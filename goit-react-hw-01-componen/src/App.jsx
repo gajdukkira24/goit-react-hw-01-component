0e914f636d3f1da37e36a4acf4828f0a502c77f1
Kira <gajdukkira232@gmail.com>
2026-02-19 18:33:33 +0200

    п

---
+++ goit-react-hw-01-componen/src/App.jsx
@@ -0,0 +1,19 @@
+import Profile from "./Profile";
+import user from "./user.json";
+
+function App() {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+}
+
+export default App;
+
